f3994f472294fc8c36fbf0f00bb898418634edf5
David Craig <david.craig2@mail.dcu.ie>
2018-02-07 16:25:43 +0000

    Replace testwm.py

--- code/testwm.py
+++ code/testwm.py
@@ -1,10 +1,23 @@
 from Xlib.display import Display
 import time
 from subprocess import call 
+from Xlib import X, XK
 
 dpy = Display()
 root = dpy.screen().root
 call("~/.fehbg",shell=True)
-time.sleep(5)
-dpy.close()
+
+dpy.screen().root.grab_key(dpy.keysym_to_keycode(XK.string_to_keysym("F1")), X.Mod1Mask, 1, X.GrabModeAsync, X.GrabModeAsync)
+
+
+def handle_event(event):
+    if event.type == X.KeyPress: 
+        dpy.close()
+
+
+    
+
+while 1:
+    event = dpy.next_event()
+    handle_event(event)
 
