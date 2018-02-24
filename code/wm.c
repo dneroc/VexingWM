6ab01c3cf751e23f9f8656f8a08aac63391c65c1
sudziub2 <benas.sudzius2@mail.dcu.ie>
2018-02-24 12:29:17 +0000

    Created a simpler layout, should be eaier to write code this way

--- code/wm.c
+++ code/wm.c
@@ -5,33 +5,24 @@
 //Max function for setting minimum window size
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 
-//XK_Alt set from keysym 
-#define XK_Alt	0xFFE9
-
 Display * disp;			//main display
 XWindowAttributes attr;	//attributes of a window
 XButtonEvent start; 	//save pointers state at the beginning
 XEvent ev;				//event variable
 
-void onMapRequest(XEvent *ev)
-{
-	
-}
 
-void handleButtRelease(XEvent *ev)
-{
+void handleButtRelease(XButtonReleasedEvent ev){
 	//reset start
-	if(ev -> type == ButtonRelease)
+	if(ev.type == ButtonRelease)
     	start.subwindow = None;
 }
 
-void handleMotion(XEvent *ev)
+void handleMotion(XMotionEvent ev)
 {
 	//If window moved, depending on button pressed, left click move = move, right click + move = resize using alt mod
-	if(start.subwindow != None)
-    {
-        int xdiff = ev -> xbutton.x_root - start.x_root;
-        int ydiff = ev -> xbutton.y_root - start.y_root;
+	if(start.subwindow != None){
+        int xdiff = ev.x_root - start.x_root;
+        int ydiff = ev.y_root - start.y_root;
         XMoveResizeWindow(disp, start.subwindow,
         attr.x + (start.button==1 ? xdiff : 0),
         attr.y + (start.button==1 ? ydiff : 0),
@@ -40,55 +31,45 @@ void handleMotion(XEvent *ev)
     }
 }
 
-void handleButton(XEvent *ev)
+void handleButton(XButtonEvent ev)
 {
 	//Sets the start of the pointer for moving it
-	if(ev -> xbutton.subwindow != None)
-        {
-            XGetWindowAttributes(disp, ev -> xbutton.subwindow, &attr);
-            start = ev -> xbutton;
+	if(ev.subwindow != None){
+            XGetWindowAttributes(disp, ev.subwindow, &attr);
+            start = ev;
         }
 }
 
 
-void handleKey(XEvent *ev)
+void handleKey(XKeyEvent ev)
 {
 	//Event to raise focused window with Alt+F2
 	//TODO:Future will have to run by clicking window, when in focus
 
-	if(ev -> xkey.state == Mod1Mask && ev -> xkey.subwindow != None && ev -> xkey.keycode == XKeysymToKeycode(disp,XK_F1))
-	{
-        XRaiseWindow(disp, ev -> xkey.subwindow);
+	if(ev.state == Mod1Mask && ev.subwindow != None && ev.keycode == XKeysymToKeycode(disp,XK_F1)){
+        XRaiseWindow(disp, ev.subwindow);
 	}
 
-	if(ev -> xkey.state == Mod1Mask && ev -> xkey.subwindow != None && ev -> xkey.keycode == XKeysymToKeycode(disp,XK_Tab))
-	{
-		system("xterm &");
+	if(ev.state == Mod1Mask && ev.subwindow != None && ev.keycode == XKeysymToKeycode(disp,XK_Tab)){
+		system("xclock &");
 	}
 
 }
+
+//Event loop for intercepting different types of events
 
 void eventLoop()
 {
 	XNextEvent(disp, &ev);
 	
-	switch(ev.type)
-	{
-		case KeyPress:		handleKey(&ev);
-		case ButtonPress:	handleButton(&ev);
-		case ButtonRelease:	handleButtRelease(&ev); //;)
-		case MotionNotify: 	handleMotion(&ev);
-		case MapRequest:	onMapRequest(&ev);
-
-
-
-
-
-
-
-
-
-
+	switch(ev.type){
+		case KeyPress:			handleKey(ev.xkey);			
+								
+		case ButtonPress:		handleButton(ev.xbutton);		
+								
+		case ButtonRelease:		handleButtRelease(ev.xbutton);	
+								
+		case MotionNotify: 		handleMotion(ev.xmotion);	
 	}
 }
 
@@ -98,6 +79,7 @@ int main(void)
 
     if(!(disp = XOpenDisplay(0x0))) return 1; //fail if can't connect
 
+	//EventMasks, only sends events of this type
 	//Send events when this combination of buttons/keys/modifiers is pressed
 	//Alt + F1
     XGrabKey(disp, XKeysymToKeycode(disp, XK_F1), Mod1Mask, DefaultRootWindow(disp), True, GrabModeAsync, GrabModeAsync);
@@ -116,12 +98,10 @@ int main(void)
     XGrabButton(disp, 3, Mod1Mask, DefaultRootWindow(disp), True,
             ButtonPressMask|ButtonReleaseMask|PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
 
-
     start.subwindow = None;
-    while(True)
-    {
+    while(True){
 		//basic X event loop
        eventLoop();
 
     }
-}
+} 
