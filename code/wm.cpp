f12f93f43c9ee5089f4722437a49bb2f303e50db
sudziub2 <benas.sudzius2@mail.dcu.ie>
2018-02-27 19:13:47 +0000

    Cleaned up the code a little bit

--- code/wm.cpp
+++ code/wm.cpp
@@ -37,30 +37,39 @@ Window parent;			//Parent for titlebar move
 //EventMasks, only sends events of this type
 void setMasks(){
 
-	//Send events when this combination of buttons/keys/modifiers is pressed
 	//Alt + F1
-    XGrabKey(disp, XKeysymToKeycode(disp, XK_F1), Mod1Mask, DefaultRootWindow(disp), True, GrabModeAsync, GrabModeAsync);
+    XGrabKey(disp, XKeysymToKeycode(disp, XK_F1), 
+	Mod1Mask, DefaultRootWindow(disp), 
+	True, GrabModeAsync, GrabModeAsync);
 
 	//Alt + F2
-	XGrabKey(disp, XKeysymToKeycode(disp, XK_F2), Mod1Mask, DefaultRootWindow(disp), True, GrabModeAsync, GrabModeAsync);
+	XGrabKey(disp, XKeysymToKeycode(disp, XK_F2), 
+	Mod1Mask, DefaultRootWindow(disp), 
+	True, GrabModeAsync, GrabModeAsync);
 
 	//Alt + F4
-	XGrabKey(disp, XKeysymToKeycode(disp, XK_F4), Mod1Mask, DefaultRootWindow(disp), True, GrabModeAsync, GrabModeAsync);
+	XGrabKey(disp, XKeysymToKeycode(disp, XK_F4), 
+	Mod1Mask, DefaultRootWindow(disp), 
+	True, GrabModeAsync, GrabModeAsync);
 	
 	//Alt + Tab
-	XGrabKey(disp, XKeysymToKeycode(disp, XK_Tab), Mod1Mask, DefaultRootWindow(disp), True, GrabModeAsync, GrabModeAsync);
+	XGrabKey(disp, XKeysymToKeycode(disp, XK_Tab), 
+	Mod1Mask, DefaultRootWindow(disp), 
+	True, GrabModeAsync, GrabModeAsync);
 	
 	//Alt + Esc
-	XGrabKey(disp, XKeysymToKeycode(disp, XK_Escape), Mod1Mask, DefaultRootWindow(disp), True, GrabModeAsync, GrabModeAsync);
-
-	//Left mouse click
-    //XGrabButton(disp, 1, AnyModifier, DefaultRootWindow(disp), True, ButtonPressMask|ButtonReleaseMask|PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
+	XGrabKey(disp, XKeysymToKeycode(disp, XK_Escape), 
+	Mod1Mask, DefaultRootWindow(disp), 
+	True, GrabModeAsync, GrabModeAsync);
 
 	//Alt + Right mouse click
-    XGrabButton(disp, 3, Mod1Mask, DefaultRootWindow(disp), True, ButtonPressMask|ButtonReleaseMask|PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
+    XGrabButton(disp, 3, Mod1Mask, DefaultRootWindow(disp), 
+	True, ButtonPressMask|ButtonReleaseMask|PointerMotionMask,
+ 	GrabModeAsync, GrabModeAsync, None, None);
 
 	//Events for reparenting 
-	XSelectInput(disp, DefaultRootWindow(disp), SubstructureRedirectMask | SubstructureNotifyMask);
+	XSelectInput(disp, DefaultRootWindow(disp),
+	SubstructureRedirectMask | SubstructureNotifyMask);
 }
 
 void handleButtRelease(XButtonReleasedEvent ev){
@@ -72,11 +81,13 @@ void handleButtRelease(XButtonReleasedEvent ev){
 void reparentWindow(Window window){
 	//Attributes of origial window
 	XGetWindowAttributes(disp, window, &attr);
-
-	//TODO: Create an interactive title bar(move/resize, etc)
+	
+	//TODO: Fix title bar only working for one window (maybe reset which title bar it is?)
+	//TODO: add buttons to title bar
 	int borderWidth = 3;
 	int colour = 0xFFF000;
 	int background = 0xFFFFFF;
+	int titleColour = 0x000FFF;	
 
 	//Setting parent window charechtaristics, last 3: border width, colour and background
 	frame = XCreateSimpleWindow(disp, DefaultRootWindow(disp), attr.x, attr.y, attr.width, attr.height + 20, borderWidth, colour, background);
@@ -86,29 +97,31 @@ void reparentWindow(Window window){
 
 	//Reparents child window
 	XReparentWindow(disp, window, frame, 0, 20);
+
 	//Displays parent window(frame)
 	XMapWindow(disp, frame);
-
-	int titleColour = 0x000FFF;	
+	
 	//Title bar
-	//TODO: change to root display so I can grab indipendantly, move along with the rest of the windows
-	title = XCreateSimpleWindow(disp, frame, attr.x, attr.y, attr.width, 20, 0, 0xFFF000, titleColour);
-
+	title = XCreateSimpleWindow(disp, frame, attr.x, attr.y,
+	attr.width, 20, 0, 0xFFF000, titleColour);
 	XMapWindow(disp, title);
+
 	//Click on the title bar event
-	XGrabButton(disp, 1, AnyModifier, title, False, ButtonPressMask|ButtonReleaseMask|PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
+	XGrabButton(disp, 1, AnyModifier, title, 
+	False, ButtonPressMask|ButtonReleaseMask|PointerMotionMask,
+	GrabModeAsync, GrabModeAsync, None, None);
 }
 
 void handleMapRequest(XMapRequestEvent ev){
 
 	reparentWindow(ev.window);
+
 	//Remaps the child window
 	XMapWindow(disp, ev.window);
 }
 
 //Fixes xterm being tiny when reparented
 void handleConfigRequest(XConfigureRequestEvent ev){
-	
 	XWindowChanges ch;
 	ch.x = ev.x;
 	ch.y = ev.y;
@@ -124,39 +137,40 @@ void handleConfigRequest(XConfigureRequestEvent ev){
 
 void handleMotion(XMotionEvent ev) {
 	//TODO: Handle resizing to include the inner window
-	//If window moved, depending on button pressed, left click move = move, right click + move = resize using alt mod
+	//TODO: Fix only one title bar being selected
 	int xdiff = ev.x_root - start.x_root;
     int ydiff = ev.y_root - start.y_root;
 
+	//Move window by dragging title bar
 	if(start.window == title){
         XMoveWindow(disp, parent,
         attr.x + (start.button==1 ? xdiff : 0),
         attr.y + (start.button==1 ? ydiff : 0));
 	}
-		
+	
+	//Resize
 	if(start.subwindow != None){
 		XResizeWindow(disp, ev.subwindow,
         MAX(100, attr.width + (start.button==3 ? xdiff : 0)),
         MAX(100, attr.height + (start.button==3 ? ydiff : 0)));
 		
     }
-	
 }
 
 void handleButton(XButtonEvent ev) {
+
 	//Sets the start of the pointer for moving it
-
 	if(ev.subwindow != None){
-			cout << "Button 3" << endl;
-            XGetWindowAttributes(disp, ev.subwindow, &attr);
-            start = ev;
-			XRaiseWindow(disp, ev.subwindow);
-        }
+		cout << "Button 3" << endl;
+        XGetWindowAttributes(disp, ev.subwindow, &attr);
+        start = ev;
+		XRaiseWindow(disp, ev.subwindow);
+    }
 
+	//For pressing on the title bar
 	if(ev.window == title){
 		Window root, *child = NULL;
 		unsigned int nchild;
-		
 		cout << "Button 1 title" << endl;
 		XQueryTree(disp, ev.window, &root, &parent, &child, &nchild);
 		XRaiseWindow(disp, parent);
@@ -164,28 +178,24 @@ void handleButton(XButtonEvent ev) {
 	}	
 }
 
-
 void handleKey(XKeyEvent ev) {
-	if(ev.state == Mod1Mask && ev.subwindow != None && ev.keycode == XKeysymToKeycode(disp,XK_F1)){
-        XRaiseWindow(disp, ev.subwindow);
-	}
-	
 	//Alt + Tab creates new xclock
-	else if(ev.state == Mod1Mask && ev.subwindow != None && ev.keycode == XKeysymToKeycode(disp,XK_Tab)){
+	if(ev.keycode == XKeysymToKeycode(disp,XK_Tab)){
 		system("xclock &");
 	}
 
 	//Alt + Escape closes window Manager
-	else if(ev.state == Mod1Mask && ev.keycode == XKeysymToKeycode(disp,XK_Escape)){
+	else if(ev.keycode == XKeysymToKeycode(disp,XK_Escape)){
 	        XCloseDisplay(disp);	
 	}
-
-
-        //Alt+F4 closes window
-	else if(ev.state == Mod1Mask && ev.keycode == XKeysymToKeycode(disp,XK_F4)){
+	
+    //Alt+F4 closes window
+	//TODO:(bug) KillClient kills the whole display
+	else if(ev.keycode == XKeysymToKeycode(disp,XK_F4)){
             XKillClient(disp, ev.window);
             return;
-        }
+    }
+}
 /*
                 Atom* supported_protocols;
                 int num_supported_protocols;
@@ -206,15 +216,11 @@ void handleKey(XKeyEvent ev) {
                }
 	}
 */
- 
- }
-
 
 void handleUnmapNotify(Window window) {
-
     Window frame = frames[window];
     XUnmapWindow(disp, frame);
-    XReparentWindow(disp, window, DefaultRootWindow(disp),0, 0);
+    XReparentWindow(disp, window, DefaultRootWindow(disp), 0, 0);
     XRemoveFromSaveSet(disp, window);
     XDestroyWindow(disp, frame);
     frames.erase(window);
@@ -227,26 +233,39 @@ void eventLoop()
 	XNextEvent(disp, &ev);
 	
 	switch(ev.type){
-		case KeyPress:			handleKey(ev.xkey);		break;
-		case ButtonPress:		handleButton(ev.xbutton);	break;
-		case ButtonRelease:		handleButtRelease(ev.xbutton);	break;
-		case MotionNotify: 		handleMotion(ev.xmotion);	break;
-		case MapRequest:		handleMapRequest(ev.xmaprequest); break;
-		case ConfigureRequest:	handleConfigRequest(ev.xconfigurerequest); break;
-                case UnmapNotify:       handleUnmapNotify(ev.xunmap.window); break;
+		case KeyPress:
+			handleKey(ev.xkey);	break;
+
+		case ButtonPress:
+			handleButton(ev.xbutton); break;
+
+		case ButtonRelease:		
+			handleButtRelease(ev.xbutton); break;
+
+		case MotionNotify: 		
+			handleMotion(ev.xmotion); break;
+
+		case MapRequest:		
+			handleMapRequest(ev.xmaprequest); break;
+
+		case ConfigureRequest:	
+			handleConfigRequest(ev.xconfigurerequest); break;
+
+        case UnmapNotify:       
+			handleUnmapNotify(ev.xunmap.window); break;
 	}
 }
 
 
-int main(int argc, char* argv[]) {
-    if(!(disp = XOpenDisplay(0x0))) return 1; //fail if can't connect
+int main(void) {
 
+	//fail if can't connect
+    if(!(disp = XOpenDisplay(0x0))) return 1;
 	setMasks();
     start.subwindow = None;
     while(True){
 		//basic X event loop		
 		eventLoop();
-
     }
 } 
 
