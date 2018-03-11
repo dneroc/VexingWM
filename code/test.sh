ed5fdfaa89d8ad808dd1dea870fa6adf05f8c587
sudziub2 <benas.sudzius2@mail.dcu.ie>
2018-03-11 16:31:34 +0000

    added tests

--- code/test.sh
+++ code/test.sh
@@ -16,16 +16,12 @@ xdotool click 1
 output=$(xdotool getmouselocation)
 rootWindow=$(echo $output | awk '{print $4}' | cut -d":" -f2)
 
-#Open terminal
+#Test1: Open terminal
 xdotool key alt+Return
 sleep 0.2
-
-#Get terminal number
 xdotool mousemove 100 10
 output=$(xdotool getmouselocation)
 terminal=$(echo $output | awk '{print $4}' | cut -d":" -f2)
-
-#Test1: Check if terminal is open
 if [ "$rootWindow" != "$terminal" ]
 then
 	count=$((count + 1))
@@ -33,7 +29,7 @@ else
 	echo "Test1: Terminal open test fail"
 fi
 
-#Test2: Check exit button
+#Test2: Exit button
 xdotool mousemove 10 10
 xdotool click 1
 xdotool mousemove 100 10
@@ -91,24 +87,100 @@ else
 	echo "Test4: Window resizing failed. Should be 674x526, is: $windowSize"
 fi
 
+#Test5: Resize window (from maximise button)
+xdotool key alt+Return
+sleep 0.1
+xdotool mousemove 30 10
+xdotool keydown alt
+xdotool mousedown 3
+xdotool mousemove 200 200
+xdotool keyup alt
+xdotool mouseup 3
+output=$(xdotool getmouselocation)
+window=$(echo $output | awk '{print $4}' | cut -d":" -f2)
+output=$(xdotool getwindowgeometry $window)
+windowSize=$(echo $output | awk '{print $8}' | cut -d":" -f2)
 
+if [ "$windowSize" == "654x526" ]
+then
+	xdotool mousemove 10 10
+	xdotool click 1
+	count=$((count + 1))
+else
+	echo "Test5: Window resizing failed. Should be 654x526, is: $windowSize"
+fi
 
-echo "$count /4 tests passed"
+#Test6: Resize window (from title bar)
+xdotool key alt+Return
+sleep 0.1
+xdotool mousemove 100 10
+xdotool keydown alt
+xdotool mousedown 3
+xdotool mousemove 200 200
+xdotool keyup alt
+xdotool mouseup 3
+output=$(xdotool getmouselocation)
+window=$(echo $output | awk '{print $4}' | cut -d":" -f2)
+output=$(xdotool getwindowgeometry $window)
+windowSize=$(echo $output | awk '{print $8}' | cut -d":" -f2)
+
+if [ "$windowSize" == "584x526" ]
+then
+	xdotool mousemove 10 10
+	xdotool click 1
+	count=$((count + 1))
+else
+	echo "Test6: Window resizing failed. Should be 654x526, is: $windowSize"
+fi
+
+#Test7: Resize window (from client window)
+xdotool key alt+Return
+sleep 0.1
+xdotool mousemove 100 100
+xdotool keydown alt
+xdotool mousedown 3
+xdotool mousemove 200 200
+xdotool keyup alt
+xdotool mouseup 3
+output=$(xdotool getmouselocation)
+window=$(echo $output | awk '{print $4}' | cut -d":" -f2)
+output=$(xdotool getwindowgeometry $window)
+windowSize=$(echo $output | awk '{print $8}' | cut -d":" -f2)
+
+if [ "$windowSize" == "584x436" ]
+then
+	xdotool mousemove 10 10
+	xdotool click 1
+	count=$((count + 1))
+else
+	echo "Test7: Window resizing failed. Should be 654x526, is: $windowSize"
+fi
+
+#Test8: Maximise button
+xdotool key alt+Return
+sleep 0.1
+xdotool mousemove 30 10
+xdotool click 1
+output=$(xdotool getmouselocation)
+window=$(echo $output | awk '{print $4}' | cut -d":" -f2)
+output=$(xdotool getwindowgeometry $window)
+windowSize=$(echo $output | awk '{print $8}' | cut -d":" -f2)
+echo $windowSize
+output=$(xdotool getwindowgeometry $rootWindow)
+rootSize=$(echo $output | awk '{print $8}' | cut -d":" -f2)
+
+if [ "$windowSize" == "$rootSize" ]
+then
+	xdotool mousemove 10 10
+	xdotool click 1
+	count=$((count + 1))
+else
+	echo "Test8: Window resizing failed. Should be $rootSize, is: $windowSize"
+fi
+
+echo "$count /8 tests passed"
 
 sleep 500
-
-#Open, move and close terminal
-xdotool key alt+Return
-sleep 0.5
-xdotool mousemove 100 10
-sleep 0.5
-xdotool mousedown 1
-xdotool mousemove 300 300 
-xdotool mouseup 1
-xdotool mousemove_relative -- -90 0
-sleep 0.5
-xdotool click 1
-
 
 #Open Dmenu and firefox
 xdotool key alt+F2
@@ -147,20 +219,6 @@ xdotool mousemove 10 10
 sleep 0.5
 xdotool click 1
 
-#Open and resize terminal on title
-xdotool key alt+Return
-sleep 0.5
-xdotool mousemove 100 10
-sleep 1 
-xdotool keydown alt
-xdotool mousedown 3
-xdotool mousemove 300 300
-sleep 0.5
-xdotool keyup alt
-xdotool mouseup 3
-xdotool mousemove 10 10
-sleep 0.5
-xdotool click 1
 
 #Open and maximise terminal
 xdotool key alt+Return
